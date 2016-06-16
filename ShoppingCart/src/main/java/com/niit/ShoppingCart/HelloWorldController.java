fecc7433a75b7cb3eb9ad19c947ab3b1fc1aa0f4
vvijaibaskar <vijaibaskar@yahoo.com>
2016-06-16 23:12:06 +0530

    MavenSpringProject

---
+++ ShoppingCart/src/main/java/com/niit/ShoppingCart/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.niit.ShoppingCart;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping("/hello")
+	public ModelAndView welcomeMessage(
+			@RequestParam(value = "name", required = false) String name) {
+		// Name of your jsp file as parameter
+		ModelAndView view = new ModelAndView("hello");
+		view.addObject("name", name);
+		return view;
+	}
+}
---
+++ ShoppingCart/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+    <h2>Hello World</h2>
+    <h3><a href="hello?name=Vijaibaskar">Click here...</a></h3>
+</body>
+</html>
